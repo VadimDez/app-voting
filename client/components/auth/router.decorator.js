f873ccc0cc4eb4d05a140fe6b8b0a3837298cf5f
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-05-06 20:29:29 +0200

    removed / updated frontend dependencies

--- client/components/auth/router.decorator.js
+++ client/components/auth/router.decorator.js
@@ -11,7 +11,7 @@ angular.module('appVotingApp.auth')
       }
 
       if(typeof next.authenticate === 'string') {
-        Auth.hasRole(next.authenticate, _.noop).then(has => {
+        Auth.hasRole(next.authenticate, () => {}).then(has => {
           if(has) {
             return;
           }
@@ -22,7 +22,7 @@ angular.module('appVotingApp.auth')
           });
         });
       } else {
-        Auth.isLoggedIn(_.noop).then(is => {
+        Auth.isLoggedIn(() => {}).then(is => {
           if(is) {
             return;
           }
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,6 @@ module.exports = function(config) {
       'client/bower_components/angular-resource/angular-resource.js',
       'client/bower_components/angular-cookies/angular-cookies.js',
       'client/bower_components/angular-sanitize/angular-sanitize.js',
-      'client/bower_components/lodash/dist/lodash.compat.js',
       'client/bower_components/angular-ui-router/release/angular-ui-router.js',
       'client/bower_components/angular-validation-match/dist/angular-validation-match.min.js',
       'client/bower_components/angular-socialshare/dist/angular-socialshare.min.js',
